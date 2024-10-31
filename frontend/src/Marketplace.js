d0a0b879085cf840a139df24a595630446c53456
KurtKobain <114251731+KurtKobain@users.noreply.github.com>
2024-10-31 17:38:59 -0400

    Create Marketplace.js

---
+++ frontend/src/Marketplace.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Marketplace = () => {
+  return (
+    <div>
+      <h1>Marketplace</h1>
+      <p>Explore items for sale.</p>
+    </div>
+  );
+};
+
+export default Marketplace;
