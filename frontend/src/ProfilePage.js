09c22259b99e904c89e96d6c5a4a284f2da41309
KurtKobain <114251731+KurtKobain@users.noreply.github.com>
2024-10-31 17:38:36 -0400

    Create ProfilePage.js

---
+++ frontend/src/ProfilePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Manage your profile here.</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
