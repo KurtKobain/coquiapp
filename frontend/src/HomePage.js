babde44b08cb6eca5001c15f1fd47f73c10b1710
KurtKobain <114251731+KurtKobain@users.noreply.github.com>
2024-10-31 17:38:09 -0400

    Create HomePage.js

---
+++ frontend/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to COQUI</h1>
+      <p>This is your personalized news feed.</p>
+    </div>
+  );
+};
+
+export default HomePage;
