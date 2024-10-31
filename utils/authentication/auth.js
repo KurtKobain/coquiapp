30718c3405645c9b65e9caa399547c394ae6cadf
KurtKobain <114251731+KurtKobain@users.noreply.github.com>
2024-10-31 17:34:40 -0400

    Create auth.js

---
+++ utils/authentication/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+// Generate Token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+// Middleware for protecting routes
+const protect = (req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (token && token.startsWith('Bearer')) {
+    try {
+      token = token.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  } else {
+    res.status(401).json({ message: 'No token provided' });
+  }
+};
+
+module.exports = { generateToken, protect };
